a7e2a3cf4e01d2fc836a4838704b8750cd1c8147
Cam Arrigo <carrigo3@gmail.com>
2019-01-23 16:41:11 -0500

    Initial Commit

---
+++ WeekTwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeekTwo
+//
+//  Created by Cameron Arrigo on 1/23/19.
+//  Copyright © 2019 Cameron Arrigo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
